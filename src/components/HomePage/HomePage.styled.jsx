a2b13415a676da360bbd496b8bc87dedc659040b
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-09-30 17:26:49 +0300

    home page styles edited

--- src/components/HomePage/HomePage.styled.jsx
+++ src/components/HomePage/HomePage.styled.jsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled'
 
 export const Title = styled.h1`
-    margin-bottom: 15px; 
-    color: #f55670;
+    margin-bottom: 15px;
+    color: #d83b55;
 
     font-weight: 600;
     font-size: 24px;
